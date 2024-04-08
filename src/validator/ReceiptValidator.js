2a66bbc1a7c9ac79f6e84403d10ec75594d41258
wicahma <teguh.dc.kusuma@gmail.com>
2024-04-08 13:43:48 +0700

    update: adding more validation for print_status

--- src/validator/ReceiptValidator.js
+++ src/validator/ReceiptValidator.js
@@ -18,15 +18,20 @@ class ReceiptValidator {
       negara_tujuan: Joi.string().max(50).not(null).required(),
       nama_lengkap: Joi.string().max(100).not(null).required(),
       usia: Joi.number().not(null),
-      jenis_kelamin: Joi.string().valid(
-        ...Object.values({
-          L: "L",
-          P: "P",
-        })
-      ).not(null),
+      jenis_kelamin: Joi.string()
+        .valid(
+          ...Object.values({
+            L: "L",
+            P: "P",
+          })
+        )
+        .not(null),
       harga: Joi.number().not(null).required(),
     });
-    const patientSchema = Joi.array().items(patientItemsSchema).not(null).required();
+    const patientSchema = Joi.array()
+      .items(patientItemsSchema)
+      .not(null)
+      .required();
 
     const schema = Joi.object({
       receipt: receiptSchema,
@@ -56,11 +61,28 @@ class ReceiptValidator {
 
   async updateReceiptTKIValidator(req, res, next) {
     const receiptSchema = Joi.object({
-      tanggal: Joi.date().required(),
-      nama_penanggungjawab: Joi.string().max(100).required(),
-      nama_sponsor: Joi.string().max(100).required(),
+      print_status: Joi.boolean().required(),
+      tanggal: Joi.when("print_status", {
+        is: Joi.boolean().valid(false),
+        then: Joi.date().required(),
+        otherwise: Joi.date().allow(null).allow(""),
+      }),
+      nama_penanggungjawab: Joi.when("print_status", {
+        is: Joi.boolean().valid(false),
+        then: Joi.string().max(100).required(),
+        otherwise: Joi.string().allow(null).allow("").required(),
+      }),
+      nama_sponsor: Joi.when("print_status", {
+        is: Joi.boolean().valid(false),
+        then: Joi.string().max(100).required(),
+        otherwise: Joi.string().allow(null).allow("").required(),
+      }),
       keterangan: Joi.string().allow(null).allow("").max(255),
-      total_pembayaran: Joi.number().required(),
+      total_pembayaran: Joi.when("print_status", {
+        is: Joi.boolean().valid(false),
+        then: Joi.number().required(),
+        otherwise: Joi.number().allow(null).allow(""),
+      }),
     });
 
     const patientItemsSchema = Joi.object({
@@ -77,7 +99,11 @@ class ReceiptValidator {
       ),
       harga: Joi.number().required(),
     });
-    const patientSchema = Joi.array().items(patientItemsSchema).not(null).required();
+    const patientSchema = Joi.when("print_status", {
+      is: false,
+      then: Joi.array().items(patientItemsSchema).not(null).required(),
+      otherwise: Joi.array().allow(null).allow("").required(),
+    });
 
     const schema = Joi.object({
       receipt: receiptSchema,
