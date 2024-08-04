d86560f673239ac4f2f8a7b3733670b651c1460c
wicahma <teguh.dc.kusuma@gmail.com>
2024-08-04 13:14:52 +0700

    update: adding cors origin value

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ app.use(
       "https://kwitansiku-dev.vercel.app",
       "https://klinikgora.vercel.app",
       "http://localhost:5173",
+      "http://127.0.0.1:5173",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     preflightContinue: false,
