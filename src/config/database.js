4df7f026ed2b32250d986362251b612b9c6ca38f
wicahma <teguh.dc.kusuma@gmail.com>
2025-01-13 01:39:33 +0700

    Update: fixing database port for local development

--- src/config/database.js
+++ src/config/database.js
@@ -1,35 +1,38 @@
-const config = require('./config');
+const config = require("./config");
 
 module.exports = {
-    development: {
-        username: config.dbUser,
-        password: config.dbPass,
-        database: config.dbName,
-        host: config.dbHost,
-        dialect: 'mysql',
-        dialectOptions: {
-            bigNumberStrings: true,
-        },
+  development: {
+    username: config.dbUser,
+    password: config.dbPass,
+    database: config.dbName,
+    dbPort: config.dbPort,
+    host: config.dbHost,
+    dialect: "mysql",
+    dialectOptions: {
+      bigNumberStrings: true,
     },
-    test: {
-        username: config.dbUser,
-        password: config.dbPass,
-        database: config.dbName,
-        host: config.dbHost,
-        dialect: 'mysql',
-        dialectOptions: {
-            bigNumberStrings: true,
-        },
+  },
+  test: {
+    username: config.dbUser,
+    password: config.dbPass,
+    database: config.dbName,
+    dbPort: config.dbPort,
+    host: config.dbHost,
+    dialect: "mysql",
+    dialectOptions: {
+      bigNumberStrings: true,
     },
-    production: {
-        username: config.dbUser,
-        password: config.dbPass,
-        database: config.dbName,
-        host: config.dbHost,
-        port: config.port,
-        dialect: 'mysql',
-        dialectOptions: {
-            bigNumberStrings: true,
-        },
+  },
+  production: {
+    username: config.dbUser,
+    password: config.dbPass,
+    database: config.dbName,
+    dbPort: config.dbPort,
+    host: config.dbHost,
+    port: config.port,
+    dialect: "mysql",
+    dialectOptions: {
+      bigNumberStrings: true,
     },
+  },
 };
